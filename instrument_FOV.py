39379044ceccc9426ed57f7a97a0a38ea665f371
Giuseppe Greco <giuseppe.greco@uniurb.it>
2016-02-07 19:24:11 +0100

    Update instrument_FOV.py

--- instrument_FOV.py
+++ instrument_FOV.py
@@ -1,4 +1,4 @@
-def instrument_FOV(FOV_base, FOV_height):
+def instrument_FOV( FOV_base, FOV_height ):
     
     """
 
@@ -15,10 +15,10 @@ def instrument_FOV(FOV_base, FOV_height):
     from astropy.io.votable import parse
     
     # download an Aladin Instrument Footprint file from GitHub
-    # or making it from http://aladin.u-strasbg.fr/footprint_editor/#
+    # or make it from http://aladin.u-strasbg.fr/footprint_editor/#
     
     # Reading the Instrument Footprint file template
-    votable = parse("footprint_GWsky2.vot")
+    votable = parse( "footprint_GWsky2.vot" )
 
     # get table in the file (FOV size)
     table = votable.get_first_table()
@@ -42,10 +42,10 @@ def instrument_FOV(FOV_base, FOV_height):
     data[3] = - FOV_base_arcsec /  2.0, - FOV_height_arcsec / 2.0
 
     # outputting a VOTable file
-    votable.to_xml('instrument_FOV.vot')
+    votable.to_xml( 'instrument_FOV.vot' )
     
     # send to aladin the instrument_FOV.vot
-    aladinSAMP.send_file('instrument_FOV.vot')
+    aladinSAMP.send_file( 'instrument_FOV.vot' )
 
     
 
