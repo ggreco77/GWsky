df5bcb50dfa0bd6aef2f839f84e76521d1b17665
Giuseppe Greco <giuseppe.greco@uniurb.it>
2016-02-07 19:19:58 +0100

    Update and rename highest_probability_pixel.py to find_highest_probability_pixel.py

--- find_highest_probability_pixel.py
+++ find_highest_probability_pixel.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-def highest_probability_pixel(infile):
+
+def find_highest_probability_pixel( sky_map ):
 
      """
 
-     Find the highest probability pixel RA[deg], DEC[deg]
+     finding the highest probability pixel RA[deg], DEC[deg].
 
      """
  
@@ -12,23 +13,23 @@ def highest_probability_pixel(infile):
      import numpy as np
 
      # read probability skymap
-     hpx = hp.read_map(infile,verbose=False)
+     hpx = hp.read_map( sky_map, verbose = False )
 
      # number of pixels
-     npix = len(hpx)
+     npix = len( hpx )
 
      # nside: resolution for the HEALPix map
-     nside = hp.npix2nside(npix)
+     nside = hp.npix2nside( npix )
 
      # pixel position
-     ipix_max = np.argmax(hpx)
-     hpx[ipix_max]
+     ipix_max = np.argmax( hpx )
+     hpx[ ipix_max ]
 
      # sky coordinate
-     theta, phi = hp.pix2ang(nside, ipix_max)
+     theta, phi = hp.pix2ang( nside, ipix_max )
 
-     ra_max = np.rad2deg(phi)
+     ra_max = np.rad2deg( phi )
 
-     dec_max = np.rad2deg(0.5 * np.pi - theta)
+     dec_max = np.rad2deg( 0.5 * np.pi - theta )
 
      print ' or insert the highest probability pixel located \n at RA =', str(( '% .5f' % ra_max))+'°', 'and Dec =',str(( '% .5f' % dec_max))+'°.' 
