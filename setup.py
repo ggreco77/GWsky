b907cdce51878f7efc491e48dab0dcfac1903e18
Giuseppe Greco <giuseppe.greco@uniurb.it>
2016-09-12 03:28:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='GWsky',
       author_email='giuseppe.greco@uniurb.it',
       license='BSD',
       url='https://github.com/ggreco77/GWsky',
-      requires=['aladin_console', 'aladinSAMP', 'astropy', 'healpy', 'pickle', 'time'],
+      requires=['astropy', 'healpy','matplotlib', 'pandas', 'numpy'],
       long_description="GWsky is an interactive Python script to generate a sequence of pointings given a specific Field of View (FoV). The script aims to split the large GW sky localization into several independent areas. It defines a sequence of FoVs from a fixed position over the sky, e.g., starting from the highest probability pixel. The results are displayed in Aladin Sky Atlas (http://aladin.u-strasbg.fr/) using the SAMPIntegratedClient class. ",
       classifiers=[
           'Development Status :: v2 - Beta',
