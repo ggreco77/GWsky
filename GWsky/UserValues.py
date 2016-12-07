1a49034f7c147fe0c48024d9ac15866529dab2de
Giuseppe Greco <giuseppe.greco@uniurb.it>
2016-12-07 03:05:29 +0100

    Update UserValues.py

--- GWsky/UserValues.py
+++ GWsky/UserValues.py
@@ -83,10 +83,10 @@ class UserInput(object):
         print ('            and check the SAMP connection:')
         print ( ' Aladin bar --> Interop --> Connect with SAMP. ')
         print('')             
-        print('  The *UserValues* module creates 3 configuration file:)
-        print('  GWsky_config, GWsky_coords and user_fov.vot.')
-        print (' The configuration file is read by the module *coverage*. ')
-        print('')
+        #print('  The *UserValues* module creates 3 configuration file:)
+        #print('  GWsky_config, GWsky_coords and user_fov.vot.')
+        #print (' The configuration file are read by the module *coverage*. ')
+        #print('')
 
     def display_SampError(self):
         print('')
