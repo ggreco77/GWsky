15daf0891e465717756fd1c17f9296e6c322bb71
Giuseppe Greco <giuseppe.greco@uniurb.it>
2018-11-10 03:48:13 +0100

    Update gwsky.py

--- GWsky/gwsky.py
+++ GWsky/gwsky.py
@@ -601,7 +601,7 @@ class UserValuesGUI:
         
         messagebox.showinfo('User Values has been initialized', message)
         
-        import coverage
+        import .coverage
 
         Utils.create_folders(folders=["Queries", "Coords", "FoV"])
         Utils.load_user_fov("GWsky_fov.vot")
